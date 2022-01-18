fb56d2cd78434acdf809cbb02c17a7e84d8486ab
Josselin <druardjosselin@MacBook-Pro-de-Druard.local>
2022-01-18 18:36:23 +0100

    Commit

---
+++ TRADING_TP3_DRUARD_Josselin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TRADING_TP3_DRUARD_Josselin
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
